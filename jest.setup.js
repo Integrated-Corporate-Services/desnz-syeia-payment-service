e019314a8825f6a4417ec3e62fb3613b9f1d8ec0
SauravkIcs2582 <Saurav.Kumar@ics.gov.uk>
2026-07-31 16:09:36 +0100

    SYEIA-2309-documentation-update (#39)

--- jest.setup.js
+++ jest.setup.js
@@ -11,10 +11,10 @@ process.env.DB_USER = 'postgres';
 process.env.DB_PASSWORD = 'dummy';
 process.env.PGSSLMODE = 'disable';
 
-process.env.GOVPAY_WEBHOOK_SIGNING_KEY = 'test-govpay-signing-key-for-tests-12345678';  
-process.env.GOVPAY_API_KEY = 'test-govpay-api-key-not-real-value';                      
-process.env.GOVPAY_API_URL = 'https://test.payments.service.gov.uk';               
-process.env.UKSBS_WEBHOOK_SIGNING_KEY = 'test-signing-key-for-tests-12345678';    
+process.env.GOVPAY_WEBHOOK_SIGNING_KEY = 'test-dummy';  
+process.env.GOVPAY_API_KEY = 'test-dummy';                      
+process.env.GOVPAY_API_URL = 'https://test.com';               
+process.env.UKSBS_WEBHOOK_SIGNING_KEY = 'test-dummy';    
 process.env.BACKEND_SERVICE_URL = 'http://localhost:3000';
 process.env.LOG_LEVEL = 'error'; 
 process.env.CALLBACK_SERVICE_ENABLED = 'true';
@@ -27,6 +27,6 @@ process.env.AWS_REGION = 'eu-west-2';
 process.env.AWS_ENDPOINT = 'http://localhost:4566'; 
 process.env.AWS_ACCESS_KEY_ID = 'test-key';
 process.env.AWS_SECRET_ACCESS_KEY = 'test-secret';
-process.env.PAYMENT_WEBHOOK_QUEUE_URL = 'http://localhost:4566/000000000000/payment-webhook-queue';
+process.env.PAYMENT_WEBHOOK_QUEUE_URL = 'http://localhost:4566/000000000000/test-queue';
 process.env.SQS_ENABLED = 'false'; 
 
